78efe3d89f602403cccebfbd9d35695d3615646a
noobmaster69jr <srashwin22@gmail.com>
2023-08-01 13:25:48 +0530

    login page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Login from './pages/Login'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
